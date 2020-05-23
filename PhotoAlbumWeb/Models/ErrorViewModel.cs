22c6c875d60df1b4b1a96f1aaf30a9f910239e97
Wilder Arredondo Torres <wildersoft@gmail.com>
2020-05-23 15:19:15 -0500

    Agregar archivos de proyecto.

---
+++ PhotoAlbumWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PhotoAlbumWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
